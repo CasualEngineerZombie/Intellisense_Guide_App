cebcb92e25fe7aac75223896de0eca06278813bb
Rian Barriga <barrigaete@gmail.com>
2019-10-20 16:22:25 +0800

    Updating the Learn Page

--- lib/Learn.dart
+++ lib/Learn.dart
@@ -4,6 +4,7 @@ class Learn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0.0,
@@ -60,14 +61,144 @@ class Learn extends StatelessWidget {
       body: PageView(
         children: <Widget>[
           Container(
-          color: Colors.green,
-        ),
-        Container(
-          color: Colors.blue,
-        ),
-        Container(
-          color: Colors.yellow,
-        ),
+            child: ListView(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Text(
+                        'Occupational Health and Safety Policies and Procedures',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                      width: 150,
+                      child: Divider(
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      'Learning Objectives:',
+                      style: TextStyle(
+                        fontFamily: 'Source Code Pro',
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Table(
+                      children: [
+                        TableRow(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Text(
+                                'After reading this INFORMATION SHEET, YOU MUST be able to:',
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontFamily: 'Poppins',
+                                ),
+                                textAlign: TextAlign.justify,
+                              ),
+                            )
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Text(
+                                '1. Identify the precautionary measures before and during assembling computer.',
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontFamily: 'Poppins',
+                                ),
+                                textAlign: TextAlign.justify,
+                              ),
+                            ),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Text(
+                                '2. Determine the ways in preventing illness and injury or death from working a computer.',
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontFamily: 'Poppins',
+                                ),
+                                textAlign: TextAlign.justify,
+                              ),
+                            ),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Text(
+                                '3. Identify common hazards and risks that usually encountered by computer technicians while working.',
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontFamily: 'Poppins',
+                                ),
+                                textAlign: TextAlign.justify,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Card(
+                      child: ExpansionTile(
+                        leading: Icon(Icons.star_border),
+                        title: Column(
+                          children: <Widget>[
+                            Text(
+                              'Occupational Health & Safety',
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                            Text(
+                              'A quick introduction to Occupational Health & Safety',
+                              style: TextStyle(fontSize: 14.0),
+                            ),
+                          ],
+                        ),
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Text(
+                              'Occupational Health and Safety (OHS) is a planned system of working to prevent illness and injury where you work by recognizing and identifying hazards and risks. Health and Safety procedure is the responsibility of all persons in the computer and technology industries. You must identify the hazards where you are working and decide how dangerous they are.',
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                fontFamily: 'Poppins',
+                              ),
+                              textAlign: TextAlign.justify,
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.blue,
+          ),
+          Container(
+            color: Colors.yellow,
+          ),
         ],
       ),
     );
